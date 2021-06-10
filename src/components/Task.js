c818e5f6e40065643533e4fff8a90ef7cadce5c9
magnitopic <magnitopic@gmail.com>
2021-06-10 23:20:54 +0200

    Small bug fix

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,8 +3,10 @@ import { FaTimes } from 'react-icons/fa'
 const Task = ({ task, onDelete, onToggle }) => {
     return (
         <div className={`task ${task.reminder ? 'reminder' : ''}`}>
-            <h3 onClick={() => onToggle(task.id)}>
-                {task.text}
+            <h3>
+                <div onClick={() => onToggle(task.id)}>
+                    {task.text}
+                </div>
                 <FaTimes
                     style={{ color: 'red', cursor: 'pointer' }}
                     onClick={() => onDelete(task.id)}
